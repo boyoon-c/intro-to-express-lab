48b01c3eb1a4796b4014beebd476448a2a8dad7c
boyoon-c <boyoonchang@gmail.com>
2021-07-12 23:47:17 -0700

    db update

--- data/racers-db.js
+++ data/racers-db.js
@@ -3,10 +3,11 @@ export {
 }
 
 const racers = [
-  {name: 'Lewis', team: 'Mercedes', _id: 125223},
-  {name: 'Max', team: 'Red Bull', _id: 127904},
-  {name: 'Sergio', team: 'Red Bull', _id: 139608},
-  {name: 'Daniel', team: 'McLaren', _id: 123456}
+  {name: 'Lewis Hamilton', team: 'Mercedes', pos: 2, _id: 44},
+  {name: 'Max Verstappen', team: 'Red Bull', pos: 1, _id: 33},
+  {name: 'Sergio Perez', team: 'Red Bull', pos: 3, _id: 11},
+  {name: 'Daniel Ricciardo', team: 'McLaren', pos: 8, _id: 3},
+  {name: 'Pierre Gasly', team: 'AlphaTauri', pos: 9, _id:10}
 ]
 
 const find = (conditions, callback) => {
@@ -16,7 +17,7 @@ const find = (conditions, callback) => {
     if (!(conditions instanceof Object)){
       throw new TypeError('Please pass in an object')
     }
-    // If the object is empty, return all the todos
+    // If the object is empty, return all the racers
     if (Object.keys(conditions).length === 0) return callback(null, racers)
 	// deal with errors
   } catch (error) {
